8aa87513c50acbfa255adebae8b552db54a213c7
Jason <l>
2018-04-24 21:44:14 +0800

    commit

---
+++ FileWatcherServiceWithTopShelf/FileWatcherServiceWithTopShelf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileWatcherServiceWithTopShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<ConverterService>(serviceInstance => {
+                    serviceInstance.ConstructUsing(() => new ConverterService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                });
+
+                serviceConfig.SetServiceName("FileWatcherService");
+                serviceConfig.SetDisplayName("File Watcher Service");
+                serviceConfig.SetDescription("Demo service use topshelf");
+
+                serviceConfig.StartManually();
+            });
+        }
+    }
+}
