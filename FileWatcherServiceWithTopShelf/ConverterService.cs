645bae1fb939ef52a14bca9c89b69b7e938f7b29
Jason <l>
2018-04-24 22:03:33 +0800

    add log

---
+++ FileWatcherServiceWithTopShelf/ConverterService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherServiceWithTopShelf
+{
+    public class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"C:\temp\a", "*_in.txt");
+            _watcher.Created += FileCreated;
+
+            _watcher.IncludeSubdirectories = false;
+            _watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            var content = File.ReadAllText(e.FullPath);
+            var uperContent = content.ToUpperInvariant();
+            var dir = Path.GetDirectoryName(e.FullPath);
+            var convertFileName = Path.GetFileName(e.FullPath) + ".converted";
+            var convertPath = Path.Combine(dir, convertFileName);
+            File.WriteAllText(convertPath, uperContent);
+            
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+            return true;
+        }
+    }
+}
---
+++ FileWatcherServiceWithTopShelf/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿use admin cmd
+change directory to 
+cd /d D:\Learning\TopShelf\FileWatcherServiceWithTopShelf\FileWatcherServiceWithTopShelf\bin\Debug
+=> FileWatcherServiceWithTopShelf help
+
